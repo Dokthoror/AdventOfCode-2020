d74d8736a32d47fa08b68c57d5e4028a3c70ec5c
Dokthoror <pascalguyguy@gmail.com>
2020-12-02 11:12:30 +0100

    Day 01

---
+++ day-01/puzzle-01.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+
+const inputData = fs.readFileSync('input-file.txt', 'utf8');
+const lines = inputData.split(/\r?\n/);
+
+
+lines.forEach(fLine => {
+    lines.forEach(sLine => {
+        if (Number(fLine) + Number(sLine) == 2020) {
+            console.log(`${fLine} * ${sLine} = ${Number(fLine) * Number(sLine)}`);
+        }
+    });
+});
---
+++ day-01/puzzle-02.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+
+const inputData = fs.readFileSync('input-file.txt', 'utf8');
+const lines = inputData.split(/\r?\n/);
+
+
+lines.forEach(fLine => {
+    lines.forEach(sLine => {
+        lines.forEach(tLine => {
+            if (Number(fLine) + Number(sLine) + Number(tLine) == 2020) {
+                console.log(`${fLine} * ${sLine} * ${Number(tLine)} = ${Number(fLine) * Number(sLine) * Number(tLine)}`);
+            }
+        })
+    })
+});
